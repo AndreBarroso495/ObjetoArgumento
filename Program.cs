5c2f710415bdf5fae37bf4e30611e0659e7e1e2b
AndreBarroso495 <73665987+AndreBarroso495@users.noreply.github.com>
2020-12-21 09:39:05 -0300

    Projeto Concluído

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ObjetoArgumento.classes;
+
+namespace ObjetoArgumento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+         Produto p1 = new Produto(1, "Iphone 12", 6000f);
+         Produto p2 = new Produto(2, "Samsung Galaxy S20", 3500f);
+         Produto p3 = new Produto(3, "Motorola Moto G9 Plus", 2000f);
+
+         Carrinho carrinho = new Carrinho();
+         carrinho.AdicionarProduto(p1);
+         carrinho.AdicionarProduto(p2);
+         carrinho.AdicionarProduto(p3);
+
+         carrinho.MostrarProdutos();
+            
+        }
+    }
+}
---
+++ classes/Produto.cs
@@ -0,0 +1,18 @@
+namespace ObjetoArgumento.classes
+{
+    public class Produto
+    {
+        public int Codigo {get; set; } 
+
+        public string Nome {get; set; }
+
+        public float Preco { get; set; }
+        
+        public Produto(int _codigo, string _nome, float _preco){
+            Codigo = _codigo;
+            Nome = _nome;
+            Preco = _preco;
+        }
+        
+    }
+}
